74c809950b49578c8907e4b354047851d1766b41
Devon Marantz <devonmarantz@gmail.com>
2019-02-07 16:16:54 -0800

    KevinIvyDevon

--- cardio.js
+++ cardio.js
@@ -21,20 +21,66 @@ const inventors = [
 const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
 
 // 1. Filter the list of inventors for those who were born in the 1500's
+const oldPeople = inventors.filter(function(inventor) {
+  return inventor.year > 1499 && inventor.year < 1600 ? inventor : ''
+})
+ console.log(oldPeople);
 
 // 2. Give us an array of the inventors' first and last names
 
+const fullName = inventors.map(function(inventor){
+  return {first: inventor.first,
+    last: inventor.last
+  }
+})
+ console.log(fullName);
+
 // 3. Sort the inventors by birthdate, oldest to youngest
+const sortedBirthdays = inventors.sort((lhs,rhs) => {
+  return lhs.year < rhs.year ? -1 : 1;
+})
+console.log(sortedBirthdays);
 
 // 4. How many years did all the inventors live?
+//Gives the amount of characters in the all the array
+const totalYears = inventors.reduce((accumulator, inventor) => {
+    return accumulator + (inventor.passed - inventor.year);
+}, 0)
+console.log(totalYears);
+
 
 // 5. Sort the inventors by years lived
+const sortedAge = inventors.sort((lhs,rhs) => {
+  return (lhs.passed - lhs.year) < (rhs.passed - rhs.year) ? 1 : -1;
+})
+console.log(sortedAge);
+
 
 // 6. Sort the people alphabetically by last name
+const sortedLastName = inventors.sort((lhs,rhs) => {
+  return lhs.last > rhs.last ? 1 : -1;
+})
+console.log(sortedLastName);
+
 
 // 7. Sum up the instances of each of these vehicles
 // prettier-ignore
 const data = ['car','car','truck','truck','bike','walk','car','van','bike','walk','car','van','car','truck'];
+
+const cars = {
+};
+
+
+  const carsCount = data.forEach(key =>
+{    if (cars.hasOwnProperty(key)) {
+      cars[key]++;
+  }
+  else {
+    cars[key]=1;
+  }
+  });
+console.log(cars);
+
 
 // 8. is at least one person 19 or older?
 // 9. is everyone 19 or older?
@@ -44,6 +90,16 @@ const team = [
   { name: 'Irv', year: 1970 },
   { name: 'Lux', year: 2015 },
 ];
+
+const legalAgeOne = team.some(function(person){
+  return (2019 - person.year) >= 19
+})
+console.log(legalAgeOne);
+
+const legalAgeEveryone = team.every(function(person){
+  return (2019 - person.year) >= 19
+})
+console.log(legalAgeEveryone);
 
 // For the following, make functions that satisfy the requirements
 // Use this comment data for testing
